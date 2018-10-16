daa8bb4fb8c7a755fa80628095b9bee69d96bcc0
John Genere <johncgenere@gmail.com>
2018-10-16 00:09:26 -0400

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -45,11 +45,15 @@ typedef ostream& (*output_function) (ostream & out);
 
 ******************************************************************************/
 
+// serves as an introductory line before the N lines of oddity
 ostream& lineBeforeOddity(ostream &out);
+
+// prints out an oddity choosen from the oddities.txt file
 ostream& johncgenere_oddity(ostream &out);
 
 int main(int argc, char* argv[])
 {
+    // Call to introductory line
     lineBeforeOddity(cout);
     // Calls to output functions
     johncgenere_oddity(cout);
@@ -57,11 +61,13 @@ int main(int argc, char* argv[])
    return 0;
 }
 
+// serves as an introductory line before the N lines of oddity
 ostream& lineBeforeOddity(ostream &out){
     out << "This is a collection of strange but true facts:" << endl << endl;
     return out;
 }
 
+// prints out an oddity choosen from the oddities.txt file
 ostream& johncgenere_oddity(ostream &out){
     out << "Why do we drive on parkways and park on driveways?" << endl;
     return out;
