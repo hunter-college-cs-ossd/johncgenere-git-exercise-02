1b03a116620a6bc797919b9621db25141160c00a
John Genere <johncgenere@gmail.com>
2018-10-15 23:12:16 -0400

    added johncgenere_oddity

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,27 @@
 /******************************************************************************
   Title          : main.cpp
-  Author         : 
-  Created on     : October  7, 2018
+  Author         : John Genere
+  Created on     : October 15, 2018
   Description    : Displays random peculiar facts
   Purpose        : To exercise collaborative source code development
   Usage          : nuttyfact
   Build with     : g++ -Wall -g -o oddities  main.cpp
-  Modifications  :
+  Modifications  : Added a introduction line before all of the oddities
+                   Added johncgenere_oddity
   Notes:
   The number N below is the number of people on each team.
   This program prints N peculiar facts, called "oddities" here.
   The main program calls N functions, each with the syntax defined
-  by the output_function typedef defined below. Each function must be 
-  named 
+  by the output_function typedef defined below. Each function must be
+  named
        username_oddity
 
   where username is the GitHub username of its author.
-  For example, 
+  For example,
       output_function  stewartweiss_oddity;
 
   Oddities are found in the file "oddities.txt" in the project source directory
- 
+
 ******************************************************************************/
 
 #include <iostream>
@@ -35,26 +36,30 @@ using namespace std;
 /******************************************************************************/
 
 // The syntax of the output function that must be used to print an oddity.
-typedef ostream& (*output_function) (const ostream & out);
+typedef ostream& (*output_function) (ostream & out);
 
 
 /******************************************************************************
-        
+
                         All Function Definitions
 
 ******************************************************************************/
 
+ostream& lineBeforeOddity(ostream &out){
+    out << "This is a collection of strange but true facts:" << endl << endl;
+    return out;
+}
 
+ostream& johncgenere_oddity(ostream &out){
+    out << "Why do we drive on parkways and park on driveways?" << endl;
+    return out;
+}
 
-
-
-
-int main(int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
-
+    lineBeforeOddity(cout);
     // Calls to output functions
+    johncgenere_oddity(cout);
 
-
-
-    return 0;
+   return 0;
 }
